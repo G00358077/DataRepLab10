d5201717f41c58adc0e78ec96a7e1c5790a12cec
GAVIN <G00358077@gmit.ie>
2020-10-22 10:48:55 +0100

    lab 3

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Content } from './components/content';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Read } from './components/read';
+import { Create } from './components/create';
 
 class App extends Component {
   render() {
@@ -24,8 +26,8 @@ class App extends Component {
           <br />
           <Switch>
             <Route path='/' component={Content} exact />
-            <Route path='/read' component={Header} exact />
-            <Route path='/create' component={Footer} exact />
+            <Route path='/read' component={Read} exact />
+            <Route path='/create' component={Create} exact />
           </Switch>
         </div>
       </Router>
