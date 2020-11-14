d87f3ff70fc97054a5b3b436c60c4cadde111a56
GAVIN <G00358077@gmit.ie>
2020-11-14 13:52:04 +0000

    first commit

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 
 export class Create extends React.Component {
 
@@ -37,9 +38,19 @@ export class Create extends React.Component {
 
     onSubmit(e){
         e.preventDefault();
-        alert("Movie: "+this.state.Title);
-        alert("Year: "+this.state.Year);
-        alert("Poster: "+this.state.Poster);
+        alert("Movie: "+this.state.Title + ", " + 
+        "Year: "+this.state.Year + ", " + 
+        "Poster: "+this.state.Poster);
+
+        const newMovie = {
+            title : this.state.Title,
+            year : this.state.Year,
+            poster : this.state.Poster
+        }
+
+        axios.post('http://localhost:4000/api/movies', newMovie)
+        .then((res)=>{console.log(res);})
+        .catch((err)=>{console.log(err);});
     }
 
     render() {
